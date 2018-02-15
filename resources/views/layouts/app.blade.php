9a8456d9a3f3faf662b54f2cd45c89c20fed74f0
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-15 19:25:40 +0100

    added button for new discussion creation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <div class="panel panel-default">
+                    <a href="{{route('discussions.create')}}" class="btn btn-primary form-control">Create a new Discussion</a>
+                    <br><br>
+                    <div class="panel panel-default">                        
                         <div class="panel-heading">
                             Channels
                         </div>
