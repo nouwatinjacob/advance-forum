8e6a58460cf78d10e9b330bec330af77b3bcfd4c
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-15 12:40:21 +0100

    --seeding admin

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            'name' => 'admin',
+            'email' => 'admin@advance-forum.com',
+            'password' => bcrypt('admin'),
+            'admin' => 1,
+            'avatar' => asset('avatars/user.jpg'),
+        ]);
+    }
+}
