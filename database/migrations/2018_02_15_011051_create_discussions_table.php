be3fb98d152a9d9977221e001d8956419d56a57f
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-16 09:45:33 +0100

    slud added to discussion

--- database/migrations/2018_02_15_011051_create_discussions_table.php
+++ database/migrations/2018_02_15_011051_create_discussions_table.php
@@ -19,6 +19,7 @@ class CreateDiscussionsTable extends Migration
             $table->integer('channel_id')->unsigned();
             $table->string('title');
             $table->text('content');
+            $table->string('slug');
             $table->timestamps();
         });
     }
