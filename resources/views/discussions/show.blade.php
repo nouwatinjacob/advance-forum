9f46fe02440feb641d1f9c37450629bbf46c7d7f
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-16 09:45:10 +0100

    code re-adjusted

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">{{ $discussion->title }}</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -17,7 +15,5 @@
                     You are logged in!
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+        
 @endsection
