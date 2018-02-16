49ebe3b5844a4ce0363e6aaaf78441517804ea5b
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-16 17:13:25 +0100

    code adjustment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    {{--  <title>{{ config('app.name', 'Laravel') }}</title>  --}}
+    <title>Advance Forum</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +30,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        Advance Forum
                     </a>
                 </div>
 
@@ -72,11 +73,19 @@
         </nav>
 
         <div class="container">
-            <div class="row">
-                @if(Auth::user())
+            <div class="row">                
                 <div class="col-md-4">
                     <a href="{{route('discussions.create')}}" class="btn btn-primary form-control">Create a new Discussion</a>
-                    <br><br>
+                   <br><br>
+                    <div class="panel panel-default">                        
+                            
+                            <div class="panel-body">
+                                <ul class="list-group">                                   
+                                    <a href="{{route('forum')}}" style="text-decoration: none;"><li class="list-group-item">Home</li></a>
+                                </ul>
+                            </div>
+                    </div>
+
                     <div class="panel panel-default">                        
                         <div class="panel-heading">
                             Channels
@@ -84,13 +93,13 @@
                         <div class="panel-body">
                             <ul class="list-group">
                                 @foreach($channels as $channel)
-                                <li class="list-group-item">{{ $channel->title }}</li>
+                                <a href="{{route('channel', ['slug' => $channel->slug])}}" style="text-decoration: none;"><li class="list-group-item">{{ $channel->title }}</li></a>
                                 @endforeach
                             </ul>
                         </div>
                     </div>
                 </div>
-                @endif
+                
                 <div class="col-md-8">
                     @yield('content')
                 </div>
