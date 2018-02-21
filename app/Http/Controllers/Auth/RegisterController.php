2099e9699de295f7be0e8c5b001a58a02a7c2c1f
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-21 13:18:39 +0100

    code refactored

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -61,7 +61,7 @@ class RegisterController extends Controller
      * @return \App\User
      */
     protected function create(array $data)
-    {
+    {        
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
