998cbd4635068e6c234c5d6a8add77365ffab3e9
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-22 22:44:01 +0100

    create admin middleware

--- app/Http/Controllers/ChannelsController.php
+++ app/Http/Controllers/ChannelsController.php
@@ -10,6 +10,11 @@ use Session;
 
 class ChannelsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
     /**
      * Display a listing of the resource.
      *
