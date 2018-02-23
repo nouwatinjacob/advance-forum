3eccfb705ec67b4f21409dda76ba237571ef4adc
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-23 16:40:21 +0100

    editing a reply

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -49,4 +49,24 @@ class RepliesController extends Controller
 
         return redirect()->back();
     }
+
+    public function edit($id)
+    {
+        return view('replies.edit', ['reply' => Reply::find($id)]);
+    }
+
+    public function update($id)
+    {
+        $this->validate(request(), [
+            'content' => 'required'
+        ]);
+
+        $reply = Reply::find($id);
+        $reply->content = request()->content;
+        $reply->save();
+
+        Session::flash('success', 'Reply Updated successfully');
+
+        return redirect()->route('discussion', ['slug' => $reply->discussion->slug]);
+    }
 }
